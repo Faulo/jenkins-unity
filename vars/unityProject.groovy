b265b07dfa30bb2664bb23736485d3121323a84e
Daniel Schulz <faulolio@gmail.com>
2023-02-17 18:26:12 +0100

    Add builds

--- vars/unityProject.groovy
+++ vars/unityProject.groovy
@@ -30,7 +30,13 @@ def call(body) {
         body()
         
         def project = "$WORKSPACE/${args.PROJECT_LOCATION}"
-    
+        
+        
+        stage('Folder (re)creation') {
+            sh 'rm -rf "$REPORTS" "$BUILDS" "$LOGS"'
+            sh 'mkdir -p "$REPORTS" "$BUILDS" "$LOGS"'
+        }
+        
         if (args.PROJECT_AUTOVERSION != "") {
             stage("Versioning") {
                 def version = callUnity "autoversion '${args.PROJECT_AUTOVERSION}' '$WORKSPACE'"
@@ -42,6 +48,24 @@ def call(body) {
             stage("Testing") {
                 callUnity "unity-tests '${project}' ${args.TEST_MODES} 1>'${args.REPORTS}/tests.xml'"
                 junit 'reports/tests.xml'
+            }
+        }
+        
+        if (args.BUILD_FOR_WINDOWS == '1') {
+            stage('Build for: Windows') {
+                callUnity "unity-build '$PROJECT' '$BUILDS/build-windows' windows 1>'$REPORTS/build-windows.xml'"
+                junit 'reports/build-windwos.xml'
+                sh 'zip -r build-windows.zip build-windows'                          
+                archiveArtifacts artifacts: 'builds/build-windows.zip'
+            }
+        }
+        
+        if (args.BUILD_FOR_LINUX == '1') {
+            stage('Build for: Linux') {
+                callUnity "unity-build '$PROJECT' '$BUILDS/build-linux' linux 1>'$REPORTS/build-linux.xml'"
+                junit 'reports/build-linux.xml'
+                sh 'zip -r build-linux.zip build-linux'                     
+                archiveArtifacts artifacts: 'builds/build-linux.zip'                
             }
         }
     }
