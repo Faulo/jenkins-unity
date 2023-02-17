568c4dd91ea058a702158999b24392aec532ea61
Daniel Schulz <faulolio@gmail.com>
2023-02-17 19:06:34 +0100

    Don't throw

--- vars/callUnity.groovy
+++ vars/callUnity.groovy
@@ -1,7 +1,3 @@
 def String call(String body) {
-    def result = sh(script: "\$COMPOSE_UNITY ${body}", returnStdout: true).trim();
-    if (sh.returnStatus != 0) {
-        throw new Exception("callUnity failed with exit code ${sh.returnStatus}")
-    }
-    return result
+    return sh(script: "\$COMPOSE_UNITY ${body}", returnStdout: true).trim();
 }
