bf5dc3ad6789d9636ac04be54ca4fb173f3a8e1e
Daniel Schulz <faulolio@gmail.com>
2023-02-17 18:56:02 +0100

    Use try

--- vars/unityProject.groovy
+++ vars/unityProject.groovy
@@ -43,59 +43,57 @@ def call(body) {
             }
         }
         
-        if (args.TEST_MODES != "") {
-            stage("Testing") {
-                callUnity "unity-tests '${project}' ${args.TEST_MODES} 1>'reports/tests.xml'"
+        try {
+            if (args.TEST_MODES != "") {
+                stage("Testing") {
+                    callUnity "unity-tests '${project}' ${args.TEST_MODES} 1>'reports/tests.xml'"
+                }
             }
-        }
-        
-        if (args.BUILD_FOR_WINDOWS == '1') {
-            stage('Build for: Windows') {
-                callUnity "unity-build '${project}' 'builds/build-windows' windows 1>'reports/build-windows.xml'"
-                sh 'zip -r build-windows.zip build-windows'                          
-                archiveArtifacts artifacts: 'builds/build-windows.zip'
+            
+            if (args.BUILD_FOR_WINDOWS == '1') {
+                stage('Build for: Windows') {
+                    callUnity "unity-build '${project}' 'builds/build-windows' windows 1>'reports/build-windows.xml'"
+                    sh 'zip -r build-windows.zip build-windows'                          
+                    archiveArtifacts artifacts: 'builds/build-windows.zip'
+                }
             }
-        }
-        
-        if (args.BUILD_FOR_LINUX == '1') {
-            stage('Build for: Linux') {
-                callUnity "unity-build '${project}' 'builds/build-linux' linux 1>'reports/build-linux.xml'"
-                sh 'zip -r build-linux.zip build-linux'                     
-                archiveArtifacts artifacts: 'builds/build-linux.zip'                
+            
+            if (args.BUILD_FOR_LINUX == '1') {
+                stage('Build for: Linux') {
+                    callUnity "unity-build '${project}' 'builds/build-linux' linux 1>'reports/build-linux.xml'"
+                    sh 'zip -r build-linux.zip build-linux'                     
+                    archiveArtifacts artifacts: 'builds/build-linux.zip'                
+                }
             }
-        }
-        
-        if (args.BUILD_FOR_MAC == '1') {
-            stage('Build for: Mac OS') {
-                callUnity "unity-build '${project}' 'builds/build-mac' mac 1>'reports/build-mac.xml'"
-                sh 'zip -r build-mac.zip build-mac'                     
-                archiveArtifacts artifacts: 'builds/build-mac.zip'                
+            
+            if (args.BUILD_FOR_MAC == '1') {
+                stage('Build for: Mac OS') {
+                    callUnity "unity-build '${project}' 'builds/build-mac' mac 1>'reports/build-mac.xml'"
+                    sh 'zip -r build-mac.zip build-mac'                     
+                    archiveArtifacts artifacts: 'builds/build-mac.zip'                
+                }
             }
-        }
-        
-        if (args.BUILD_FOR_WEBGL == '1') {
-            stage('Build for: WebGL') {
-                callUnity "unity-module-install '${project}' webgl 1>'reports/install-webgl.xml'"
-                callUnity "unity-method '${project}' Slothsoft.UnityExtensions.Editor.Build.WebGL 'builds/build-webgl' 1>'reports/build-webgl.xml'"
-                sh 'zip -r build-webgl.zip build-webgl'                     
-                archiveArtifacts artifacts: 'builds/build-webgl.zip'
-                publishHTML([
-                   allowMissing: false,
-                   alwaysLinkToLastBuild: false,
-                   keepAll: false,
-                   reportDir: 'builds/build-webgl',
-                   reportFiles: 'index.html',
-                   reportName: 'WebGL Build',
-                   reportTitles: '',
-                   useWrapperFileDirectly: true
-               ])
+            
+            if (args.BUILD_FOR_WEBGL == '1') {
+                stage('Build for: WebGL') {
+                    callUnity "unity-module-install '${project}' webgl 1>'reports/install-webgl.xml'"
+                    callUnity "unity-method '${project}' Slothsoft.UnityExtensions.Editor.Build.WebGL 'builds/build-webgl' 1>'reports/build-webgl.xml'"
+                    sh 'zip -r build-webgl.zip build-webgl'                     
+                    archiveArtifacts artifacts: 'builds/build-webgl.zip'
+                    publishHTML([
+                       allowMissing: false,
+                       alwaysLinkToLastBuild: false,
+                       keepAll: false,
+                       reportDir: 'builds/build-webgl',
+                       reportFiles: 'index.html',
+                       reportName: 'WebGL Build',
+                       reportTitles: '',
+                       useWrapperFileDirectly: true
+                   ])
+                }
             }
-        }
-        
-        post {
-            always {
-                junit 'reports/*.xml'
-            }
+        } finally {
+            junit 'reports/*.xml'
         }
     }
 }
