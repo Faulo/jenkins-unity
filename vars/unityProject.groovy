df5b2316026fcfbac19909bd2c36a3e5a9f95938
Daniel Schulz <faulolio@gmail.com>
2023-02-19 17:24:09 +0100

    Fix dir

--- vars/unityProject.groovy
+++ vars/unityProject.groovy
@@ -61,13 +61,16 @@ def call(body) {
 	].contains('1');
 	def deployAny = args.DEPLOYMENT_BRANCHES.contains(env.BRANCH_NAME)
 
-	dir(args.LOCATION) {
-		if (versionAny) {
-			stage("Auto-Versioning") {
-				def version = callUnity "autoversion '${args.AUTOVERSION}' '$WORKSPACE'"
-				callUnity "unity-project-version '${project}' set '${version}'"
-			}
+	if (versionAny) {
+		stage("Auto-Versioning") {
+			def version = callUnity "autoversion '${args.AUTOVERSION}' '$WORKSPACE'"
+			callUnity "unity-project-version '${project}' set '${version}'"
 		}
+	}
+
+
+	dir(reports) {
+		deleteDir()
 
 		if (testAny) {
 			stage("Testing: ${args.TEST_MODES}") {
@@ -77,106 +80,104 @@ def call(body) {
 		}
 
 		if (buildAny) {
-			dir(reports) {
-				if (args.BUILD_FOR_WINDOWS == '1') {
-					stage('Building for: Windows') {
-						callUnity "unity-build '${project}' '${reports}/build-windows' windows 1>'${reports}/build-windows.xml'"
-						junit(testResults: 'build-windows.xml')
-						callShell 'zip -r build-windows.zip build-windows'
-						archiveArtifacts(artifacts: 'build-windows.zip')
-					}
+			if (args.BUILD_FOR_WINDOWS == '1') {
+				stage('Building for: Windows') {
+					callUnity "unity-build '${project}' '${reports}/build-windows' windows 1>'${reports}/build-windows.xml'"
+					junit(testResults: 'build-windows.xml')
+					callShell 'zip -r build-windows.zip build-windows'
+					archiveArtifacts(artifacts: 'build-windows.zip')
 				}
+			}
 
-				if (args.BUILD_FOR_LINUX == '1') {
-					stage('Building for: Linux') {
-						callUnity "unity-build '${project}' '${reports}/build-linux' linux 1>'${reports}/build-linux.xml'"
-						junit(testResults: 'build-linux.xml')
-						callShell 'zip -r build-linux.zip build-linux'
-						archiveArtifacts(artifacts: 'build-linux.zip')
-					}
+			if (args.BUILD_FOR_LINUX == '1') {
+				stage('Building for: Linux') {
+					callUnity "unity-build '${project}' '${reports}/build-linux' linux 1>'${reports}/build-linux.xml'"
+					junit(testResults: 'build-linux.xml')
+					callShell 'zip -r build-linux.zip build-linux'
+					archiveArtifacts(artifacts: 'build-linux.zip')
 				}
+			}
 
-				if (args.BUILD_FOR_MAC == '1') {
-					stage('Building for: MacOS') {
-						callUnity "unity-build '${project}' '${reports}/build-mac' mac 1>'${reports}/build-mac.xml'"
-						junit(testResults: 'build-mac.xml')
-						callShell 'zip -r build-mac.zip build-mac'
-						archiveArtifacts(artifacts: 'build-mac.zip')
-					}
+			if (args.BUILD_FOR_MAC == '1') {
+				stage('Building for: MacOS') {
+					callUnity "unity-build '${project}' '${reports}/build-mac' mac 1>'${reports}/build-mac.xml'"
+					junit(testResults: 'build-mac.xml')
+					callShell 'zip -r build-mac.zip build-mac'
+					archiveArtifacts(artifacts: 'build-mac.zip')
 				}
+			}
 
-				if (args.BUILD_FOR_WEBGL == '1') {
-					stage('Building for: WebGL') {
-						callUnity "unity-module-install '${project}' webgl 1>'${reports}/install-webgl.xml'"
-						junit(testResults: 'install-webgl.xml')
-						callUnity "unity-method '${project}' Slothsoft.UnityExtensions.Editor.Build.WebGL '${reports}/build-webgl' 1>'${reports}/build-webgl.xml'"
-						junit(testResults: 'build-webgl.xml')
-						callShell 'zip -r build-webgl.zip build-webgl'
-						archiveArtifacts(artifacts: 'build-webgl.zip')
-						publishHTML([
-							allowMissing: false,
-							alwaysLinkToLastBuild: false,
-							keepAll: false,
-							reportDir: 'build-webgl',
-							reportFiles: 'index.html',
-							reportName: 'WebGL Build',
-							reportTitles: '',
-							useWrapperFileDirectly: true
-						])
-					}
+			if (args.BUILD_FOR_WEBGL == '1') {
+				stage('Building for: WebGL') {
+					callUnity "unity-module-install '${project}' webgl 1>'${reports}/install-webgl.xml'"
+					junit(testResults: 'install-webgl.xml')
+					callUnity "unity-method '${project}' Slothsoft.UnityExtensions.Editor.Build.WebGL '${reports}/build-webgl' 1>'${reports}/build-webgl.xml'"
+					junit(testResults: 'build-webgl.xml')
+					callShell 'zip -r build-webgl.zip build-webgl'
+					archiveArtifacts(artifacts: 'build-webgl.zip')
+					publishHTML([
+						allowMissing: false,
+						alwaysLinkToLastBuild: false,
+						keepAll: false,
+						reportDir: 'build-webgl',
+						reportFiles: 'index.html',
+						reportName: 'WebGL Build',
+						reportTitles: '',
+						useWrapperFileDirectly: true
+					])
 				}
+			}
 
-				if (args.BUILD_FOR_ANDROID == '1') {
-					stage('Building for: Android') {
-						callUnity "unity-module-install '${project}' android 1>'${reports}/install-android.xml'"
-						junit(testResults: 'install-android.xml')
-						callUnity "unity-method '${project}' Slothsoft.UnityExtensions.Editor.Build.Android '${reports}/build-android.apk' 1>'${reports}/build-android.xml'"
-						junit(testResults: 'build-android.xml')
-						callShell 'zip -r build-android.zip build-android.apk'
-						archiveArtifacts(artifacts: 'build-android.zip')
-					}
+			if (args.BUILD_FOR_ANDROID == '1') {
+				stage('Building for: Android') {
+					callUnity "unity-module-install '${project}' android 1>'${reports}/install-android.xml'"
+					junit(testResults: 'install-android.xml')
+					callUnity "unity-method '${project}' Slothsoft.UnityExtensions.Editor.Build.Android '${reports}/build-android.apk' 1>'${reports}/build-android.xml'"
+					junit(testResults: 'build-android.xml')
+					callShell 'zip -r build-android.zip build-android.apk'
+					archiveArtifacts(artifacts: 'build-android.zip')
 				}
+			}
 
-				if (deployAny) {
-					if (args.DEPLOY_TO_STEAM == '1') {
-						stage('Deploying to: Steam') {
-							if (currentBuild.currentResult != "SUCCESS") {
-								error "Current result is '${currentBuild.currentResult}', skipping deployment."
-							}
+			if (deployAny) {
+				if (args.DEPLOY_TO_STEAM == '1') {
+					stage('Deploying to: Steam') {
+						if (currentBuild.currentResult != "SUCCESS") {
+							error "Current result is '${currentBuild.currentResult}', skipping deployment."
+						}
 
-							callUnity "steam-buildfile '${reports}' '${reports}' ${args.STEAM_ID} ${args.STEAM_DEPOTS} ${args.STEAM_BRANCH} 1>'${reports}/deploy-steam.vdf'"
-							withCredentials([
-								usernamePassword(credentialsId: args.STEAM_CREDENTIALS, usernameVariable: 'STEAM_CREDS_USR', passwordVariable: 'STEAM_CREDS_PSW')
-							]) {
-								callShell "steamcmd +login \$STEAM_CREDS_USR \$STEAM_CREDS_PSW +run_app_build '${reports}/deploy-steam.vdf' +quit"
-							}
+						callUnity "steam-buildfile '${reports}' '${reports}' ${args.STEAM_ID} ${args.STEAM_DEPOTS} ${args.STEAM_BRANCH} 1>'${reports}/deploy-steam.vdf'"
+						withCredentials([
+							usernamePassword(credentialsId: args.STEAM_CREDENTIALS, usernameVariable: 'STEAM_CREDS_USR', passwordVariable: 'STEAM_CREDS_PSW')
+						]) {
+							callShell "steamcmd +login \$STEAM_CREDS_USR \$STEAM_CREDS_PSW +run_app_build '${reports}/deploy-steam.vdf' +quit"
 						}
 					}
+				}
 
-					if (args.DEPLOY_TO_ITCH == '1') {
-						stage('Deploying to: itch.io') {
-							if (currentBuild.currentResult != "SUCCESS") {
-								error "Current result is '${currentBuild.currentResult}', skipping deployment."
+				if (args.DEPLOY_TO_ITCH == '1') {
+					stage('Deploying to: itch.io') {
+						if (currentBuild.currentResult != "SUCCESS") {
+							error "Current result is '${currentBuild.currentResult}', skipping deployment."
+						}
+
+						withCredentials([
+							string(credentialsId: args.ITCH_CREDENTIALS, variable: 'BUTLER_API_KEY')
+						]) {
+							if (args.BUILD_FOR_WINDOWS == '1') {
+								callShell "butler push --if-changed build-windows ${args.ITCH_ID}:windows-x64"
 							}
-
-							withCredentials([
-								string(credentialsId: args.ITCH_CREDENTIALS, variable: 'BUTLER_API_KEY')
-							]) {
-								if (args.BUILD_FOR_WINDOWS == '1') {
-									callShell "butler push --if-changed build-windows ${args.ITCH_ID}:windows-x64"
-								}
-								if (args.BUILD_FOR_LINUX == '1') {
-									callShell "butler push --if-changed build-linux ${args.ITCH_ID}:linux-x64"
-								}
-								if (args.BUILD_FOR_MAC == '1') {
-									callShell "butler push --if-changed build-mac ${args.ITCH_ID}:mac-x64"
-								}
-								if (args.BUILD_FOR_WEBGL == '1') {
-									callShell "butler push --if-changed build-webgl ${args.ITCH_ID}:html"
-								}
-								if (args.BUILD_FOR_ANDROID == '1') {
-									callShell "butler push --if-changed build-android.apk ${args.ITCH_ID}:android"
-								}
+							if (args.BUILD_FOR_LINUX == '1') {
+								callShell "butler push --if-changed build-linux ${args.ITCH_ID}:linux-x64"
+							}
+							if (args.BUILD_FOR_MAC == '1') {
+								callShell "butler push --if-changed build-mac ${args.ITCH_ID}:mac-x64"
+							}
+							if (args.BUILD_FOR_WEBGL == '1') {
+								callShell "butler push --if-changed build-webgl ${args.ITCH_ID}:html"
+							}
+							if (args.BUILD_FOR_ANDROID == '1') {
+								callShell "butler push --if-changed build-android.apk ${args.ITCH_ID}:android"
 							}
 						}
 					}
