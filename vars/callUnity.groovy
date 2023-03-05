25e345472f30fc9f027266a203d2cb7c0b2ae330
Daniel Schulz <faulolio@gmail.com>
2023-03-05 01:39:03 +0100

    Update callUnity.groovy

--- vars/callUnity.groovy
+++ vars/callUnity.groovy
@@ -1,3 +1,7 @@
-def String call(String body) {
-	return callShellStdout("\$COMPOSE_UNITY ${body}");
+def String call(String script) {
+	if (isUnix()) {
+		return sh(script: "$COMPOSE_UNITY ${body}", returnStdout: true).trim();
+	} else {
+		return bat(script: "%COMPOSE_UNITY% ${body}", returnStdout: true).trim();
+	}
 }
