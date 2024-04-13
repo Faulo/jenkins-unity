5d172e0c50518572f59c78e1aa15b47b562fe2de
Daniel Schulz <faulolio@gmail.com>
2024-04-13 18:43:09 +0200

    use bat instead of powershell

--- vars/callShell.groovy
+++ vars/callShell.groovy
@@ -2,6 +2,6 @@ def void call(String script) {
 	if (isUnix()) {
 		sh(script: script, encoding: 'UTF-8');
 	} else {
-		powershell(script: script, encoding: 'UTF-8');
+		bat(script: script, encoding: 'UTF-8');
 	}
 }
