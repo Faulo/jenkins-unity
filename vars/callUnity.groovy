c0e19efd575c86401bb0c39f9e242af96757ef29
Daniel Schulz <faulolio@gmail.com>
2024-04-14 12:17:02 +0200

    feat: use UTF-8

--- vars/callUnity.groovy
+++ vars/callUnity.groovy
@@ -6,7 +6,7 @@ def String call(String body, String file = "") {
 	def result = callComposer("exec ${body}");
 
 	if (file != "") {
-		writeFile(file: file, text: result);
+		writeFile(file: file, text: result, encoding: "UTF-8");
 	}
 
 	return result;
