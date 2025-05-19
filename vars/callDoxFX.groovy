cc5caeebe2eae1b4b3747af7d8a07af45abf2c12
Daniel Schulz <faulolio@gmail.com>
2025-05-19 11:22:11 +0200

    fix: docfx restore

--- vars/callDoxFX.groovy
+++ vars/callDoxFX.groovy
@@ -1,6 +1,7 @@
 def String call() {
 	if (isUnix()) {
 		// docker overlay magic prevents updating docfx, so we have to assume it's there.
+		callShell "dotnet tool restore"
 		callShell "dotnet tool run docfx"
 	} else {
 		def isInstalled = callShellStdout("dotnet tool list -g").contains("docfx")
