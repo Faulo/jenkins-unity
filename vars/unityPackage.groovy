7657ee16a5e526126ae5e852a10e01eafeb96891
Daniel Schulz <faulolio@gmail.com>
2025-06-07 14:53:17 +0200

    fix: canonicalPath

--- vars/unityPackage.groovy
+++ vars/unityPackage.groovy
@@ -195,7 +195,7 @@ def call(Map args) {
 								def envOverrides = []
 
 								if (env.UNITY_EMPTY_MANIFEST) {
-									def resolved = new File(env.UNITY_EMPTY_MANIFEST).absolutePath
+									def resolved = new File(pwd(), env.UNITY_EMPTY_MANIFEST).canonicalPath
 									envOverrides << "UNITY_EMPTY_MANIFEST=${resolved}"
 									echo "Setting 'UNITY_EMPTY_MANIFEST' to '${resolved}'"
 								}
