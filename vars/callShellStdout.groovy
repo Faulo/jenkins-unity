71c03944774daf7eba7ed249c8dc7c113b3d7e0b
Daniel Schulz <faulolio@gmail.com>
2025-06-11 20:32:12 +0200

    fix: ignore return status

--- vars/callShellStdout.groovy
+++ vars/callShellStdout.groovy
@@ -1,7 +1,7 @@
 def String call(String script) {
 	echo "> ${script}";
 	if (isUnix()) {
-		return sh(script: script, encoding: 'UTF-8', returnStdout: true).trim();
+		return sh(script: script + ' || true', encoding: 'UTF-8', returnStdout: true).trim();
 	} else {
 		// https://stackoverflow.com/questions/2095088/error-when-calling-3rd-party-executable-from-powershell-when-using-an-ide
 		return powershell(returnStdout: true, encoding: 'UTF-8', script: '''
