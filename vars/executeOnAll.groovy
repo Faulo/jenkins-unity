01d1a3af9612a1832c83919b9488d577d07df22d
Daniel Schulz <faulolio@gmail.com>
2025-06-17 20:06:50 +0200

    feat: executeOnAll

---
+++ vars/executeOnAll.groovy
@@ -0,0 +1,44 @@
+def call(Closure<Boolean> predicate, Closure<Void> payload) {
+	executeOnAll(predicate, false, payload)
+}
+
+def call(Closure<Boolean> predicate, Boolean parallel, Closure<Void> payload) {
+	def nodes = []
+	Jenkins.instance.getNodes().each { node ->
+		if (node.getComputer()?.isOnline() && predicate(node)) {
+			nodes.add(node.getNodeName())
+		}
+	}
+
+	if (parallel) {
+		def parallelStages = [:]
+		for (String nodeName in nodes) {
+			parallelStages[nodeName] = {
+				stage("Switching to: ${nodeName}") {
+					node(nodeName) {
+						stage(env.NODE_NAME) {
+							payload.call(env.NODE_NAME)
+						}
+					}
+				}
+			}
+		}
+
+		parallel parallelStages
+	} else {
+		while (!nodes.isEmpty()) {
+			def nodeExpression = nodes.collect { name -> "\"${name}\"" }.join(' || ')
+			echo "Next free node will be selected from: ${nodeExpression}"
+
+			stage("Switching to: ${nodeExpression}") {
+				node("${nodeExpression}") {
+					nodes.remove(env.NODE_NAME)
+
+					stage(env.NODE_NAME) {
+						payload.call(env.NODE_NAME)
+					}
+				}
+			}
+		}
+	}
+}
