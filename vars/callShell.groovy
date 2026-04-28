8ffe6bd24edcbc8f87620f73882a80741ad3bf29
Daniel Schulz <faulolio@gmail.com>
2026-04-28 17:19:21 +0200

    fix: powershell

--- vars/callShell.groovy
+++ vars/callShell.groovy
@@ -1,19 +1,16 @@
 def call(String script, Boolean echoScript = false) {
-	if (echoScript) {
-		echo "> ${script}";
-	}
+    if (echoScript) {
+        echo "> ${script}"
+    }
 
-	if (isWindows()) {
-		// https://stackoverflow.com/questions/2095088/error-when-calling-3rd-party-executable-from-powershell-when-using-an-ide
-		powershell(encoding: 'UTF-8', label: 'powershell -- ' + script, script: '''
-		    $ErrorActionPreference = 'Continue'
-		    $WarningPreference = 'Continue'
-		    $VerbosePreference = 'Continue'
-		    $DebugPreference = 'Continue'
-		    $InformationPreference = 'Continue'
+    if (isWindows()) {
+        if (!echoScript) {
+            echo "+ ${script}"
+        }
 
-		''' + script + ' 2>&1');
-	} else {
-		sh(script: script, encoding: 'UTF-8', label: 'sh');
-	}
+        // https://stackoverflow.com/questions/2095088/error-when-calling-3rd-party-executable-from-powershell-when-using-an-ide
+        powershell(encoding: 'UTF-8', label: 'powershell -- ' + script, script + ' 2>&1')
+    } else {
+        sh(script: script, encoding: 'UTF-8', label: 'sh')
+    }
 }
