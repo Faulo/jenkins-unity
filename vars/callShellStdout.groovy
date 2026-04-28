c334a50ba9c83ef283f87dbd77fe821c495acbe0
Daniel Schulz <faulolio@gmail.com>
2026-04-28 17:56:50 +0200

    fix: powershell

--- vars/callShellStdout.groovy
+++ vars/callShellStdout.groovy
@@ -4,6 +4,10 @@ def call(String script, Boolean echoScript = false) {
     }
 
     if (isWindows()) {
+        if (!echoScript) {
+            echo "+ ${script}"
+        }
+
         // https://stackoverflow.com/questions/2095088/error-when-calling-3rd-party-executable-from-powershell-when-using-an-ide
         return powershell(returnStdout: true, encoding: 'UTF-8', label: 'powershell -- ' + script, script: script + ' 2>$null').trim()
     } else {
