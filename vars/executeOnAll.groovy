93f06ee2cc488c70c1982c5dd9d4da8bd19e7b82
Daniel Schulz <faulolio@gmail.com>
2026-05-15 13:11:51 +0200

    fix: jenkins serialization

--- vars/executeOnAll.groovy
+++ vars/executeOnAll.groovy
@@ -1,53 +1,62 @@
 import jenkins.model.Jenkins
 
 def call(Closure<Boolean> predicate, Closure<Void> payload) {
-	executeOnAll(predicate, false, payload)
+    call(predicate, false, payload)
 }
 
 def call(Closure<Boolean> predicate, Boolean runParallel, Closure<Void> payload) {
-	def nodes = []
+    def nodes = findNodeNames(predicate)
 
-	def jenkins = Jenkins.getInstanceOrNull()
+    if (runParallel) {
+        def parallelStages = [:]
+        for (String n in nodes) {
+            def nodeName = n
+            parallelStages[nodeName] = {
+                stage(nodeName) {
+                    node(nodeName) {
+                        payload.call(nodeName)
+                    }
+                }
+            }
+        }
 
-	if (jenkins) {
-		if (jenkins.getNumExecutors() > 0 && predicate(jenkins)) {
-			nodes.add('built-in')
-		}
-		jenkins.getNodes().each { node ->
-			if (node.getComputer()?.isOnline() && predicate(node)) {
-				nodes.add(node.getNodeName())
-			}
-		}
-	}
+        parallel parallelStages
+    } else {
+        while (!nodes.isEmpty()) {
+            def nodeExpression = nodes.collect { name -> "\"${name}\"" }.join(' || ')
+            echo "Next free node will be selected from: ${nodeExpression}"
 
-	if (runParallel) {
-		def parallelStages = [:]
-		for (String n in nodes) {
-			def nodeName = n
-			parallelStages[nodeName] = {
-				stage(nodeName) {
-					node(nodeName) {
-						payload.call(nodeName)
-					}
-				}
-			}
-		}
+            stage("Switching to: ${nodeExpression}") {
+                node("\"${nodeExpression}\"") {
+                    nodes.remove(env.NODE_NAME)
 
-		parallel parallelStages
-	} else {
-		while (!nodes.isEmpty()) {
-			def nodeExpression = nodes.collect { name -> "\"${name}\"" }.join(' || ')
-			echo "Next free node will be selected from: ${nodeExpression}"
+                    stage(env.NODE_NAME) {
+                        payload.call(env.NODE_NAME)
+                    }
+                }
+            }
+        }
+    }
+}
 
-			stage("Switching to: ${nodeExpression}") {
-				node("\"${nodeExpression}\"") {
-					nodes.remove(env.NODE_NAME)
+private List<String> findNodeNames(Closure<Boolean> predicate) {
+    def nodes = []
 
-					stage(env.NODE_NAME) {
-						payload.call(env.NODE_NAME)
-					}
-				}
-			}
-		}
-	}
+    def jenkins = Jenkins.getInstanceOrNull()
+
+    if (jenkins == null) {
+        return nodes
+    }
+
+    if (jenkins.getNumExecutors() > 0 && predicate.call(jenkins)) {
+        nodes.add('built-in')
+    }
+
+    for (def currentNode in jenkins.getNodes()) {
+        if (currentNode.getComputer()?.isOnline() && predicate.call(currentNode)) {
+            nodes.add(currentNode.getNodeName())
+        }
+    }
+
+    return nodes
 }
