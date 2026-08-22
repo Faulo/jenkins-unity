7b9b14d36165a247710df7ca0d05173b6386023f
Daniel Schulz (Codex) <faulolio@gmail.com>
2026-08-22 16:43:24 +0200

    fix: declare agent-free package pipeline explicitly
    Refs: #4 https://github.com/Faulo/jenkins-unity/issues/4

--- vars/unityPackagePipeline.groovy
+++ vars/unityPackagePipeline.groovy
@@ -24,7 +24,7 @@ void call(UnityPackagePipelineOptions pipelineOptions) {
     PreparedUnityPackage preparedPackage
 
     pipeline {
-        agent none
+        agent('none')
 
         options {
             disableConcurrentBuilds()
